6caf9e57ab6ae0de712cec56814ef023299011c5
demetre3goisashvili <demetregois@gmail.com>
2024-02-01 06:34:56 -0800

    second commit

---
+++ day 1/leasson 2.py
@@ -0,0 +1,13 @@
+name = "demetre"
+surname = "goisashvili"
+age = 14 
+me = "me"
+mqvia = "mqvia"
+var = "var"
+wlis = "wlis"
+
+
+
+
+print( me + " " + mqvia + " " + name + " " + surname + " " + me + " " + var + " " + str(age) + " " + wlis)
+
