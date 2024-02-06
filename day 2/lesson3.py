61d3404bdf691fe0ef15fefde5c714abd409c731
demetre3goisashvili <demetregois@gmail.com>
2024-02-06 08:46:42 -0800

    third commit

--- day 2/lesson3.py
+++ day 2/lesson3.py
@@ -1,1 +1,4 @@
-print("გავიცანი და დავუმეგობრდი: gio gagnidze data diasamidze demetre ekseulidze nikoloz iobashvili  " ) 
+print("გავიცანი და დავუმეგობრდი: gio gagnidze, data diasamidze, demetre ekseulidze, nikoloz iobashvili,   " ) #vin gavicani
+
+
+print("რას ვფიქრობ GOAზე: chemi azrit GOA aris msoplioshi sauketeso spero swavlistvis da ganvitarebistvis ak yvela dagexmareba rodesac gchirdeba zogadad dzalian megobruli speroa da dzalian mogvwons chveni mentori batoni nika" ) 
