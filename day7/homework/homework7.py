23b79481205e21a7461c08e9fab0f0aa552ea281
demetre3goisashvili <demetregois@gmail.com>
2024-02-25 01:19:41 -0800

    seventh commit

---
+++ day7/homework/homework7.py
@@ -0,0 +1,21 @@
+#str magaliti
+print("demetre" " " "aris" " " "GOA-s" " " "moswavle" "is" " aris" "14" " " "wlis")
+
+num1 = int(input("please enter your pin"))
+num2 = float(input("please enter the number of pi"))
+
+print(num1 + num2)
+
+
+print(input("please enter your name"))
+print(input("please enter your password"))
+
+print("Access Granted")
+
+
+
+
+
+
+
+
