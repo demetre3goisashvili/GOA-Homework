59f35bf206c239c37d97e4c623e6ad48181ce062
demetre3goisashvili <demetregois@gmail.com>
2024-02-26 09:20:42 -0800

    ninth commit

---
+++ day8/classwork/classwork8.py
@@ -0,0 +1,32 @@
+print(True or False)
+#sworia
+print(True or True)
+#sworia
+print(True and True)
+#sworia
+print(True and False)
+#arasworia
+
+#da/and operatorisas orive unda iyos swori rom miigo swori pasuxi 
+
+#an/or operatoris shemtxvevashi tu ertia swori sul sworia tu orive araswori sul araswori tu orive swori sul swori
+
+is_registered = True
+is_android = False
+
+print(is_registered and is_android)
+
+if is_registered or is_android:
+    print("Access Granted")
+else:
+    print("Atleast one of the option needs to be true")
+
+
+num = int(input("Please enter number: "))
+
+if num > 5 and num % 2 == 0:
+    print("Your entered number is correct")
+else:
+    print("Bye Bye get Mogged")
+
+# elif aris else if sxva shemtxvevashi
---
+++ day8/homework/homework8.py
@@ -0,0 +1,14 @@
+#davaleba
+
+print(True or False)
+print(True or True)
+print(False or False)
+
+print(True and False)
+print(True and True)
+print(False and False)
+
+print(10 > 4 and 10 < 9)
+print(123 > 55 or 623 > 1273 )
+print(64 > 12 or 2 > 1)
+print(124 > 21 and 362 < 3721)
