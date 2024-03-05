4893c7e12a609696c5d776368a5e68539d561bcd
demetre3goisashvili <noobdemetre@gmail.com>
2024-03-06 02:26:12 +0400

    eleventh commit

---
+++ day10/classwork/classwork10.py
@@ -0,0 +1,59 @@
+#for loop aris chveni brdzaneba rom gavushvat ganmeorebit ramdenjerac vetybit
+
+#iteration aris gameoreba
+#indentation aris dashoreba kodsa da kides shoris 
+
+#saklaso davaleba
+
+for i in range(10):
+    print(i)
+    print("demetre")
+  
+
+   
+     
+#1
+for g in range(20):
+    print(g)
+#2
+for e in range(30):
+    print(e)
+#3
+for a in range(1,31):
+    print(a)
+#4
+for d in range(1,10):
+    print(d)
+
+#kvadrati
+for p in range(1,10):
+    print(p * p)
+#gayopa
+for f in range(1,21):
+    print(f / 2)
+#mimateba
+result = 5  
+for v in range(1,11):
+    result = result + v
+
+print(result)    
+
+
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+         
