f10539cafa2d5586a67d06b4a92c510bc3552c28
demetre3goisashvili <noobdemetre@gmail.com>
2024-03-10 21:47:40 +0400

    twelvth commit

---
+++ day11/classwork/classwork11.py
@@ -0,0 +1,36 @@
+num1 = 10
+while num1 <= 20:
+    print(num1)
+    # num1 = num1 + 1
+    num1 += 2
+
+num2 = int(input("Enter number: "))
+num3 = 0
+while num3<num2:
+    print(num3)
+    num3 += 1
+
+x = 20
+x -= 10
+
+c = 5
+c += 3
+
+a = 3
+a *= 5
+
+d = 10
+d /= 5
+
+
+#!= ar udris
+
+print (5 == 5)
+print (5 == 7)
+print (5 != 7)
+print (5 != 5)
+print (5 <= 5)
+print (5 >= 5)
+
+
+
