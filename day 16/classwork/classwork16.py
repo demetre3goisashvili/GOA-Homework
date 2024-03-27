ffaf6e4f527943dcc40bb574b969e2205856789a
demetre3goisashvili <noobdemetre@gmail.com>
2024-03-27 14:49:11 +0400

    seventeenth commit

---
+++ day 16/classwork/classwork16.py
@@ -0,0 +1,19 @@
+
+
+cars = ["Mercedes", "Supra", "Toyota","Hyundai"]
+movies = ["Dune"," Breaking Bad", "Star Wars", "Spider Man"]
+Sports = ["Football", "Basketball", "Rugby", "Hockey"]
+
+print(cars[3])
+print(movies[2])
+print(Sports[1])
+
+name_surname = ["Demetre", "Goisashvili"]
+print(name_surname[1], name_surname[0])
+
+age = int(input("please enter your age: "))
+email = input("please enter your email")
+
+list = [age, email]
+print(list)
+
