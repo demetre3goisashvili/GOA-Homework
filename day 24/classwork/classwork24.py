85ecf87f928b777cae3c1fde46a44925346134f6
demetre3goisashvili <noobdemetre@gmail.com>
2024-04-28 14:45:48 +0400

    twentysixth commit

---
+++ day 24/classwork/classwork24.py
@@ -0,0 +1,15 @@
+def greet(username):
+    print("Hello", username)
+
+name = input("Please enter your name: ")
+greet(name)
+
+def add(ragaca, ragaca1):
+    print(ragaca + ragaca1)
+add(15, 16)
+
+
+
+
+
+
