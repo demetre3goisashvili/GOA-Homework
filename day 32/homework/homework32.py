e7069ec5ef884b0b5e5febe61e7c7a266d0142e7
demetre3goisashvili <noobdemetre@gmail.com>
2024-05-26 16:19:08 +0400

    twenty-third commit

--- day 32/homework/homework32.py
+++ day 32/homework/homework32.py
@@ -1,11 +1,45 @@
-def manual_index(num_list, num_search):
-    if num_search not in num_list:
-        return -1
-    #ak davwere es im shemtxvevashi tu sadziebeli mnishvnelovba ar ikneba siashi
-    for i in range(0 , len(num_list)):
-        if num_search == num_list[i]:
-            return i
+def manual_index(numbers_list, search_value):
+    smth = 0
+    for i in numbers_list:
+        if i == search_value:
+            return smth
+        smth += 1
+    return -1
+    
+print(manual_index([1,2,3,4,5,6,7,8,9,10] , 9))
+
+def manual_min(skibidi):
+    minvalue = skibidi[0]
+    for num in skibidi:
+        if minvalue > num:
+            minvalue = num
+        return minvalue
+
+    
+print(manual_min([1,2,3,4,5,6,7,8,9]))
+
+def manual_max(lst):
+    maxvalue = lst[0]
+    for num in lst:
+        if num < maxvalue:
+            maxvalue = num
+    return maxvalue
+    
+
+print(manual_max([1,2,3,4,5,6,7,8,9,10]))
+
+#danarcheni cota ver gavige
+
+
+
+
+
+
+
+
+
+    
+
         
-manual_index("jemali", "jemali")
-
-
+        
+        
