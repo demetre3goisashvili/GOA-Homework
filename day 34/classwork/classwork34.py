392f268b9235f0e962dfbb8400f9a4faa34ef4ce
demetre3goisashvili <noobdemetre@gmail.com>
2024-05-29 14:35:47 +0400

    thirty-fourth commit

---
+++ day 34/classwork/classwork34.py
@@ -0,0 +1,51 @@
+#find mushaobs mxolod stringebze, tu ar moidzebna stringshi find-it is rasac vedzebt mashin daabrunebs -1
+#append aris rom listshi chavamatot ragac
+#len-is meshveobit vigebt tu ramdeni character aris stringshi
+#insertis meshveobit chven shevdzlebt sadac gvinda ik chavwerot listshi rame items.insert(0, green) 0 aris indexi sadac gvinda chavsvat da meore green aris ra unda chavsvat
+#aseve insertis dros rodesac vsvavt konkretul rames listshi romel indexzec iyo wina ragac sanam chavamatebdit gadava shemdeg indexsze
+#pop-is meshveobit chven shegvidzlia wavshalot raime elementi siidan tu ar chavweret pop(ak) araperi mashin avtomaturad ishleba bolo elementi
+#rect() am function-is meshveobit vitvlit areals da perimetrs. custom function aris
+# def greet(name="Guest"):
+#   print("Welcome", name)
+# greet()
+# greet("john")
+
+#ak shegvidzlia parametrshive chavwerot mnishvneloba name-is magram tu gvinda shegvidzlia shevcvalot
+
+#1) შექმენით ფუნქცია რომესლაც გადაეცემა რიცხვების სია, თვენ კი უნდა დააბრუნოთ ლუწების რაოდენობა ფუნქციიდან.
+
+# def even_check(listn):
+#     for i in listn:
+#         if i % 2 == 0:
+#             print(i)
+# even_check([1,2,3,4,5,6,7,8,9,10])
+
+#2) შექმენით ფუნქცია რომელსაც გადაეცემა სია და for ციკლის გამოყენებით დაითვალეთ რამდენი ელემენტია სიაში.
+
+# def len_check(number):
+#     count = 0
+#     for i in number:
+#         count += 1
+#     return count
+        
+        
+# len_check([1,2,3,4,5,6,7,8,9,10])
+
+#3) შექმენით სია რომელსაც გადაეცემა რიცხვების სია, დააბრუნეთ ფუნქციიდან ყველა 4 ის ჯერადი რიცხვის ჯამი
+
+# def len_check(num):
+#     for i in num:
+#         if i % 2 == 0:
+#             print(i)
+# len_check([4,12,15,13,23,24])
+
+#4)შექმენით ფუნქცია სახელად manual_count რომელსაც გადაეცემა ორი მნიშვნელობა, პირველი სია და მეორე დასათvლერლი მნიშვნელობა, თვქენი დავალებაა დაითალოთ რამდენჯერ მეორდება სიაში კონკრეტული დასათვლელი მნიშნელობა
+
+
+
+
+
+
+    
+
+        
