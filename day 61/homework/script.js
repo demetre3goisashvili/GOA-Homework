02edcc06b77ac3162140b073a4f93f1714cadf00
demetre3goisashvili <noobdemetre@gmail.com>
2024-09-03 23:21:54 +0400

    sixty second commit

--- day 61/homework/script.js
+++ day 61/homework/script.js
@@ -20,5 +20,43 @@ const calculator = {
 
 console.log(calculator.calculate(15, 16))
 
+const harryPotter = {
+    title: "Harry Potter",
+    author: "J.K Rowling",
+    pages: 3407
+}
+
+const theBible = {
+    title: "The Holy Bible",
+    author: "Unknown",
+    pages: 1200
+}
+
+const theLordOfTheRings = {
+    title: "The lord of the rings",
+    author: "J. R. R. Tolkien",
+    pages: 1137
+
+}
+
+function createAnimal(name, sound) {
+    this.name = name
+    this.sound = sound
+    this.makeSound = function() {
+        console.log(this.sound)
+    }
+    this.changeName = function(changedName) {
+        this.name = changedName
+    }
+}
+
+const dog = new createAnimal("Dog", "Woof")
+dog.makeSound()
+
+const cat = new createAnimal("Cat", "Meow")
+cat.makeSound()
+
+
+
 
 
---
+++ day 62/classwork/script.js
@@ -0,0 +1,10 @@
+for(let i = 20; i < 51; i ++){
+    console.log(i)
+}
+
+i = 30
+
+while (i < 61) {
+    console.log(i)
+    i ++
+}
