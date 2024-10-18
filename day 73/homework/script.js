9871bc4795c12ea0ff3452db922b29497fdc5170
demetre3goisashvili <noobdemetre@gmail.com>
2024-10-18 22:09:45 +0400

    seventy-third commit

---
+++ day 73/homework/script.js
@@ -0,0 +1,60 @@
+class Animal {
+    constructor(name, species,age ){
+        this.name = name;
+        this.species = species;
+        this.age = age;
+    }
+    sound(){
+        console.log(this.name, "makes this sound");
+    }
+    details(){
+        console.log("Name: ", this.name);
+        console.log("Species: ", this.species);
+        console.log("Age: ", this.age, "Years old");
+    }
+}
+
+let dog = new Animal("Tobi", "Dog", "8");
+Animal.sound();
+
+class Lion extends Animal {
+    sound(){
+
+    }
+}
+
+
+class Car {
+    constructor(company, model, year){
+        this.company = company;
+        this.model = model;
+        this.year = year;
+    }
+    details(){
+        console.log("The company of the car is: ", this.company);
+        console.log("The model of the car is: ", this.model);
+        console.log("The year this car was made in is: ", this.year);
+    }
+}
+
+let mercedes = new Car("Mercedes", "E-class", "2022");
+Car.details();
+
+class Human{
+    constructor(name,lastname, age, address){
+        this.name = name
+        this.lastname = lastname
+        this.age = age
+        this.address = address
+    }
+    details(){
+        console.log("This humans name is: ", this.name)
+        console.log("This humans last name is: ", this.lastname)
+        console.log("This humans age is: ", this.age)
+        console.log("This humans address is: ", this.address)
+    }
+}
+
+
+
+
