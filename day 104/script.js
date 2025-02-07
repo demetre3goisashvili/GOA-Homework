30e7ead0ed92cdbe0b2283a794eec17c7a826a9f
demetre3goisashvili <noobdemetre@gmail.com>
2025-02-07 21:09:38 +0400

    105-th commit

---
+++ day 104/script.js
@@ -0,0 +1,12 @@
+function sum(num){
+    total = 0
+    for (let i = 0; i < num.length; i++){
+    total += num[i]
+    }
+    }
+
+
+
+// string = "saasasrasfasf"
+// print(string[::-1])
+// es aris py
---
+++ day 105/classwork/script.js
@@ -0,0 +1,31 @@
+// function func(text){
+//     let masivi = [true,true]
+//     for (let i = 0; i < text.length; i++){
+//     if (i % 2 == 0) and (text[i] % 2 == 0){
+//     masivi.push(true)
+//     }
+//     else if (i % 2 !== 0) and (text[i] % 2 !== 0){
+//     masivi.push(true)
+//     }
+//     else{
+//     masivi.push(false)
+//     }
+//     return masivi
+//     }
+//     }
+
+
+// def points(games):
+//     for i in games:
+//         number = int(i)
+//         for num in range(number):
+//             if number[0] > number[1]:
+//                 return 'win'
+//             elif number[0] < number[1]:
+//                 return 'lose'
+//             else:
+//                 return 'tie'
+        
+        
+        
+    
