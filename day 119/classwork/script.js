365efc48327cedc7579ca390319ed76a49416296
demetre3goisashvili <noobdemetre@gmail.com>
2025-04-02 22:27:54 +0400

    119-th commit

---
+++ day 119/classwork/script.js
@@ -0,0 +1,25 @@
+// result = [1,3,4]
+
+// function oriveMasivshiRoa (arr1, arr2){
+// for (let i = 0; i < arr1.length; i++){
+// for (let j = 0; j < arr2.length; j++){
+// if arr1[i] == arr2[j]{
+// if arr1[i] not in result{
+// result.append(arr1[i])
+// }
+// }
+// }
+// }
+// return result
+// }
+
+// [1,2,3,4]
+// [1,5,4,3]
+
+// for num in range(2,101):
+//     prime = True
+//     for i in range(2,num):
+//         if (num%i==0):
+//             prime = False
+//     if prime:
+//     //    print (num)
